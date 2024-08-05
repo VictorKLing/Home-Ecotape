369946f4038aac046400ccd6ef64b1bf7ba30fc4
Victor Kling <victorkling.p@gmail.com>
2024-08-05 17:06:25 -0300

    formulário e footer responsivo

---
+++ js/footer-responsivo.js
@@ -0,0 +1,18 @@
+// menu suspenso footer
+const perguntas = document.querySelectorAll(".menu-suspenso button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
